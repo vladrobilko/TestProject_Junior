5d0ef82ed507ffa113cde81eea35f113b2763fdf
EkaterinaMedIdea <144023754+EkaterinaMedIdea@users.noreply.github.com>
2023-09-04 15:19:33 +0300

    Add files via upload

---
+++ ClinicApp/HelperClasses/RegexpHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClinicApp
+{
+    static public class RegexpHelper
+    {
+        //checking if a patient name is correct
+        public static bool IsNameValid(this string name)
+        {
+            Regex regex = new Regex(@"([А-ЯЁ][а-яё]+[\-\s]?){3,}");
+            return regex.IsMatch(name);
+        }
+
+    }
+}
