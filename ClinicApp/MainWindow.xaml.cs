3b2fbeced0c45a2a61a198a9339f35c7d9a93f08
EkaterinaMedIdea <144023754+EkaterinaMedIdea@users.noreply.github.com>
2023-09-04 15:14:52 +0300

    Add files via upload

---
+++ ClinicApp/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using ClinicApp.XamlPages;
+using System.Threading;
+
+namespace ClinicApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Content = new ListOfPatientsPage(mainFrame);
+        }
+
+        private void btnAddNewPatient_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Content = new AddModifyPatient();
+        }
+        private void btnListOfPatients_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Content = new ListOfPatientsPage(mainFrame);
+        }
+
+    }
+}
