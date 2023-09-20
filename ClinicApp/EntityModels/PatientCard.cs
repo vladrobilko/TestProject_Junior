ee768ba432f63e3734e57d63bea2f18498b22818
vladrobilko <vladrobilko@gmail.com>
2023-09-20 16:59:20 +0400

    add Age column to ListsOfPatientsPage with calculated age from current day

--- ClinicApp/EntityModels/PatientCard.cs
+++ ClinicApp/EntityModels/PatientCard.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClinicApp.HelperClasses;
 
 namespace ClinicApp.EntityModels
 {
@@ -22,5 +24,7 @@ namespace ClinicApp.EntityModels
         public String Address { get; set; }
         public String PhoneNumber { get; set; }
         public List<Request> Requests { get; set; }
+        [NotMapped]
+        public string Age => DateOfBirth.GetAgeFromDate();
     }
 }
--- ClinicApp/HelperClasses/DateTimeHelper.cs
+++ ClinicApp/HelperClasses/DateTimeHelper.cs
@@ -8,5 +8,31 @@ namespace ClinicApp.HelperClasses
         {
             return dateTime > DateTime.UtcNow;
         }
+
+        public static string GetAgeFromDate(this DateTime dateOfBirthday)
+        {
+            DateTime today = DateTime.Today;
+
+            int months = today.Month - dateOfBirthday.Month;
+            int years = today.Year - dateOfBirthday.Year;
+
+            if (today.Day < dateOfBirthday.Day)
+            {
+                months--;
+            }
+
+            if (months < 0)
+            {
+                years--;
+                months += 12;
+            }
+
+            int days = (today - dateOfBirthday.AddMonths((years * 12) + months)).Days;
+
+            return string.Format("{0} {1}, {2} {3} и {4} {5}",
+                years, (years == 1) ? "год" : "лет",
+                months, (months == 1) ? "месяц" : "месяцев",
+                days, (days == 1) ? "день" : "дней");
+        }
     }
 }
